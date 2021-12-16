3b746590cb62b5b016d5e6e082866f698d801d2d
polisota <polisota@mail.ru>
2021-12-16 13:48:29 +0300

    hw lesson2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+
+    private Vector3 _direction;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+        Debug.Log(_direction);
+        //transform.LookAt(moveDirection + transform.position);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + _direction * Time.fixedDeltaTime * Speed;
+    }
+    
+
+
+}
